d3f28af0c47bab5b85a75d137822f7135c06310b
Nyan <vektordiod@gmail.com>
2024-10-18 15:44:39 +0300

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { SurveyModule } from './survey/survey.module';
 import { QuestionModule } from './question/question.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AnswerModule } from './answer/answer.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -14,17 +14,17 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const uri = configService.get<string>('DATABASE_URL');
-        return {
-          uri,
-        };
-      },
-    }),
-
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: async (configService: ConfigService) => {
+    //     const uri = configService.get<string>('DATABASE_URL');
+    //     return {
+    //       uri,
+    //     };
+    //   },
+    // }),
+    MongooseModule.forRoot('mongodb+srv://vercel-user:4GMAMPTbV14ez7EU@nest-test.u2p1v.mongodb.net/?retryWrites=true&w=majority&appName=nest-test'),
     UserModule,
     SurveyModule,
     QuestionModule,
