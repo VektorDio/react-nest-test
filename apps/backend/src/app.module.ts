ea79bafdbff807e09dd394417b6bb21328f27abe
Nyan <vektordiod@gmail.com>
2024-10-18 12:53:34 +0300

    test

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,26 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { SurveyModule } from './survey/survey.module';
-import { QuestionModule } from './question/question.module';
-import { ConfigModule } from '@nestjs/config';
-import { AnswerModule } from './answer/answer.module';
-import { AuthModule } from './auth/auth.module';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { UserModule } from './user/user.module';
+// import { SurveyModule } from './survey/survey.module';
+// import { QuestionModule } from './question/question.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { AnswerModule } from './answer/answer.module';
+// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.DATABASE_URL),
-    UserModule,
-    SurveyModule,
-    QuestionModule,
-    AnswerModule,
-    AuthModule,
-  ],
+  // imports: [
+  //   ConfigModule.forRoot({
+  //     isGlobal: true,
+  //   }),
+  //   MongooseModule.forRoot(process.env.DATABASE_URL),
+  //   UserModule,
+  //   SurveyModule,
+  //   QuestionModule,
+  //   AnswerModule,
+  //   AuthModule,
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
