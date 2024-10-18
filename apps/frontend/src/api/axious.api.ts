6eb8b251e0d07a4ef102a1cc7d62e7a6b86ccf37
Nyan <vektordiod@gmail.com>
2024-10-18 15:17:47 +0300

    test

--- apps/frontend/src/api/axious.api.ts
+++ apps/frontend/src/api/axious.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    // baseURL: process.env.VITE_API_URL,
-    baseURL: 'https://react-nest-test-backend.vercel.app'
+    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: 'https://react-nest-test-backend.vercel.app'
 })
